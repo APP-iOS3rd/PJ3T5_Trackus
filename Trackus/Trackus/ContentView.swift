a60b01baefa3199f2db14d92b24d2096f70a29df
kwonseokki <seokki0706@gmail.com>
2024-01-17 01:33:45 +0900

    initial commit

---
+++ Trackus/Trackus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trackus
+//
+//  Created by SeokKi Kwon on 2024/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trackus/Trackus/TrackusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackusApp.swift
+//  Trackus
+//
+//  Created by SeokKi Kwon on 2024/01/17.
+//
+
+import SwiftUI
+
+@main
+struct TrackusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
