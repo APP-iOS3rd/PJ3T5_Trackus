432b841b6fb3e8c9aa6312ebd38aca3b0bd8cdf7
Teoler <rlawjdghks7462@naver.com>
2024-01-19 13:10:48 +0900

    Typography 글꼴적용 입니다. 타이틀 서브타이틀 본문 타이틀 본문 입니다. struct MyTypography {
        //타이틀
        static func title(text: String) -> Text {
            return Text(text)
                .font(Font.system(size: 28, weight: .bold))
        }
        //서브 타이틀
        static func subtitle(text: String) -> Text {
            return Text(text)
                .font(Font.system(size: 20, weight: .semibold))
        }
        //본문 타이틀
        static func bodytitle(text: String) -> Text {
            return Text(text)
                .font(Font.system(size: 17, weight: .semibold))
        }
        //본문
        static func body(text: String) -> Text {
            return Text(text)
                .font(Font.system(size: 15, weight: .regular))
                .foregroundColor(.gray)
        }
    }
    사용은 다음과 같습니다
            VStack {
                //예시
                MyTypography.title(text: "트랙어스 TrackUs")
                MyTypography.subtitle(text: "트랙어스 TrackUs")
                MyTypography.bodytitle(text: "트랙어스 TrackUs")
                MyTypography.body(text: "트랙어스 TrackUs")
    
            }
    텍스트만 입력하면 자동으로 적용되도록 만들었습니다

---
+++ Trackus/Trackus/Sources/Commons/Extensions/TranslationHexColor.swift
@@ -0,0 +1,18 @@
+//
+//  TranslationHexColor.swift
+//  Trackus
+//
+//  Created by 김정환 on 1/19/24.
+//
+
+import SwiftUI
+
+struct TranslationHexColor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TranslationHexColor()
+}
---
+++ Trackus/Trackus/Sources/Commons/Items/Typographys.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct MyTypography {
+    
+    //타이틀
+    static func title(text: String) -> Text {
+        return Text(text)
+            .font(Font.system(size: 28, weight: .bold))
+    }
+    //서브 타이틀
+    static func subtitle(text: String) -> Text {
+        return Text(text)
+            .font(Font.system(size: 20, weight: .semibold))
+    }
+    //본문 타이틀
+    static func bodytitle(text: String) -> Text {
+        return Text(text)
+            .font(Font.system(size: 17, weight: .semibold))
+    }
+    //본문
+    static func body(text: String) -> Text {
+        return Text(text)
+            .font(Font.system(size: 15, weight: .regular))
+            .foregroundColor(.gray)
+    }
+}
+
+struct Typographys: View {
+    var body: some View {
+        
+        VStack {
+            //예시
+            MyTypography.title(text: "트랙어스 TrackUs")
+            MyTypography.subtitle(text: "트랙어스 TrackUs")
+            MyTypography.bodytitle(text: "트랙어스 TrackUs")
+            MyTypography.body(text: "트랙어스 TrackUs")
+
+        }
+    }
+}
+
+#Preview {
+    Typographys()
+}
