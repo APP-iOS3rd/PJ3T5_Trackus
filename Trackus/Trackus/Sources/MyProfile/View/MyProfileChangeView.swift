b8245ff8b4fe17bbae24efad972093caa1a69476
sooohi <68193613+sooohi@users.noreply.github.com>
2024-01-21 20:14:27 +0900

    feat : MyProfileView 전반적인 UI 구성

---
+++ Trackus/Trackus/Sources/MyProfile/View/MyProfileChangeView.swift
@@ -0,0 +1,15 @@
+//
+//  MyProfileChangeView.swift
+//  Trackus
+//
+//  Created by 박소희 on 1/21/24.
+//
+
+import SwiftUI
+
+struct MyProfileChangeView: View {
+    var body: some View {
+        Text("프로필 변경 화면")
+            .navigationBarTitle("프로필 변경", displayMode: .inline)
+    }
+}
--- Trackus/Trackus/Sources/MyProfile/View/MyProfileView.swift
+++ Trackus/Trackus/Sources/MyProfile/View/MyProfileView.swift
@@ -9,10 +9,144 @@ import SwiftUI
 
 struct MyProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // 상단 바
+                HStack {
+                    Text("설정 및 개인정보")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding()
+                    
+                    Spacer()
+                    
+                    Image(systemName: "gear")
+                        .font(.system(size: 20))
+                        .padding()
+                }
+                
+                // 프로필 정보
+                ProfileInfo()
+                
+                // 사용자 정보
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(height: 80)
+                    .foregroundColor(Color.gray.opacity(0.2))
+                    .padding(.horizontal)
+                    .overlay(
+                        HStack {
+                            UserDetailItemView(title: "170cm", content: "키")
+                            Spacer()
+                            UserDetailItemView(title: "65kg", content: "몸무게")
+                            Spacer()
+                            UserDetailItemView(title: "25세", content: "나이")
+                        }
+                    )
+                
+                // 설정과 개인정보 항목
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("운동")
+                        .font(.headline)
+                        .padding(.horizontal)
+                    ListSettingsItem(title: "러닝기록")
+                    Divider()
+                    
+                    Text("서비스")
+                        .font(.headline)
+                        .padding(.horizontal)
+                    ListSettingsItem(title: "이용약관")
+                    ListSettingsItem(title: "오픈소스/라이센스")
+                    Divider()
+                    
+                    Text("고객지원")
+                        .font(.headline)
+                        .padding(.horizontal)
+                    ListSettingsItem(title: "자주묻는 질문Q&A")
+                    ListSettingsItem(title: "문의하기")
+                    Divider()
+                    
+                    Text("우리팀을 응원하기")
+                        .font(.headline)
+                        .padding(.horizontal)
+                    ListSettingsItem(title: "프리미엄 결제하기")
+                    
+                }
+                .padding(.top, 20)
+                
+                Spacer()
+            }
+            .navigationBarHidden(true)
+        }
     }
 }
+
+struct ProfileInfo: View {
+    var body: some View {
+        HStack {
+            // 프로필 이미지
+            Image("userface")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .padding(.trailing, 8)
+                .clipShape(Circle())
+            
+            // 이름
+            VStack(alignment: .leading, spacing: 4) {
+                Text("이름")
+                    .font(.headline)
+            }
+            
+            Spacer()
+            
+            // 프로필 변경 버튼
+            NavigationLink(destination: MyProfileChangeView()) {
+                Text("프로필 변경>")
+                    .foregroundColor(.blue)
+                    .font(.subheadline)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.top, 20)
+    }
+}
+
+struct UserDetailItemView: View {
+    var title: String
+    var content: String
     
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.blue)
+            Text(content)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+        .padding()
+    }
+}
+
+struct ListSettingsItem: View {
+    var title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.headline)
+            
+            Spacer()
+            
+            Image(systemName: "chevron.right")
+                .font(.system(size: 18))
+                .foregroundColor(.gray)
+        }
+        .padding(.horizontal)
+    }
+}
+
+
+
 #Preview {
     MyProfileView()
 }
