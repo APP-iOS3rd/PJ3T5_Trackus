76800789076abe9a6f9741570dad66539c69f2e5
github Choe-ju <github juwon3415@gmail.com>
2024-01-23 05:18:47 +0900

    fix: 회원가입 정보 입력 View 수정

--- Trackus/Trackus/Sources/Login/View/SignUpView.swift
+++ Trackus/Trackus/Sources/Login/View/SignUpView.swift
@@ -37,7 +37,8 @@ struct SignUpView: View {
         TUCanvas.CustomCanvasView {
             switch viewModel.flow {
             case .nickname:
-                NickNameView()
+                AgeGenderView()
+                //NickNameView()
                     .environmentObject(viewModel)
             case .image:
                 ImageView()
@@ -47,10 +48,13 @@ struct SignUpView: View {
                     .environmentObject(viewModel)
             case .ageGender:
                 AgeGenderView()
+                    .environmentObject(viewModel)
             case .runningStyle:
                 RunningStyleView()
+                    .environmentObject(viewModel)
             case .daily:
                 DailyGoalView()
+                    .environmentObject(viewModel)
             }
         }
         .animation(.easeInOut, value: viewModel.flow)
@@ -120,6 +124,8 @@ struct ImageView:View {
                 VStack {
                     Button(action: {
                         // 입력값 nil 처리 -> flow 다음차례
+                        viewModel.user.profileImageUrl = nil
+                        viewModel.flow = .physical
                     }, label: {
                         MyTypography.bodytitle(text: "건너뛰기")
                     })
@@ -192,13 +198,13 @@ struct ImageView:View {
     }
 }
 
-// MARK: - 신체 정보 입력
+// MARK: - 신체 정보 입력 View
 struct PhysicalView: View {
     @EnvironmentObject var viewModel: SignUpViewModel
-    @State private var height: Int = 150
-    @State private var weight: Int = 50
     @State private var isHeightPickerPresented = false
     @State private var isWeightPickerPresented = false
+    @State private var height: Int = 170
+    @State private var weight: Int = 60
     
     let heights = 120..<230 // 키 범위
     let weights = 20..<150 // 체중 범위
@@ -211,6 +217,9 @@ struct PhysicalView: View {
                 VStack {
                     Button(action: {
                         // 입력값 nil 처리 -> flow 다음차례
+                        viewModel.user.height = nil
+                        viewModel.user.weight = nil
+                        viewModel.flow = .ageGender
                     }, label: {
                         MyTypography.bodytitle(text: "건너뛰기")
                     })
@@ -220,30 +229,62 @@ struct PhysicalView: View {
             VStack{
                 Description(image: Image(systemName: "heart"), title: "신체 정보 입력", description: "러닝 기록 분석, 칼로리 소모량 등 더욱 정확한 러닝 데이터 분석을 위해서 다음 정보가 필요합니다.")
                 VStack {
-                    Text("Height: \(height) cm")
-                        .foregroundColor(.white)
+                    HStack{
+                        MyTypography.bodytitle(text: "키")
+                            .frame(width: 50)
+                            
+                        BoxModifier{
+                            if let height = viewModel.user.height {
+                                Text("\(height)")
+                            }else{
+                                Text("-")
+                            }
+                        }
                         .onTapGesture {
                             isHeightPickerPresented = true
                         }
                         .sheet(isPresented: $isHeightPickerPresented) {
                             PickerSheet(title: "키", unit: "cm",values: heights, selectedValue: $height)
-                            //.presentationDetents([.height(250)])
                         }
+                        .onChange(of: height) { setHeight in
+                            viewModel.user.height = setHeight
+                        }
+                        MyTypography.bodytitle(text: "cm")
+                            .frame(width: 50)
+                    }
+                    .padding(8)
                     
                     // Weight
-                    Text("Weight: \(weight) kg")
-                        .foregroundColor(.white)
+                    
+                    HStack{
+                        MyTypography.bodytitle(text: "몸무게")
+                            .frame(width: 50)
+                        BoxModifier{
+                            if let weight = viewModel.user.weight {
+                                Text("\(weight)")
+                            }else{
+                                Text("-")
+                            }
+                        }
                         .onTapGesture {
                             isWeightPickerPresented = true
                         }
                         .sheet(isPresented: $isWeightPickerPresented) {
                             PickerSheet(title: "몸무게", unit: "kg",values: weights, selectedValue: $weight)
                         }
+                        .onChange(of: weight) { setWeight in
+                            viewModel.user.weight = setWeight
+                        }
+                        MyTypography.bodytitle(text: "kg")
+                            .frame(width: 50)
+                    }
+                    .padding(8)
                 }
                 .padding(.top, 40)
                 
                 Spacer()
-                TUButton(active: false, buttonText: "다음으로") {
+                TUButton(active: viewModel.user.height != nil && viewModel.user.weight != nil, buttonText: "다음으로") {
+                    viewModel.flow = .ageGender
                     
                 }
             }
@@ -252,7 +293,7 @@ struct PhysicalView: View {
     }
 }
 
-// MARK: - 연령대 및 신채정보
+// MARK: - 연령대 및 신채정보 View
 
 struct AgeGenderView: View {
     @EnvironmentObject var viewModel: SignUpViewModel
@@ -261,6 +302,12 @@ struct AgeGenderView: View {
     private let title = "연령대 및 성별"
     private let description = "연령대 정보를 선택하면 더욱 정확한 러닝 데이터 및 칼로리 소모량을 측정할 수 있습니다."
     
+    @State private var isAgePickerPresented = false
+    @State private var age = 2 // user에 대입할때 * 10 해서 반환
+    private var ages = 1..<8
+    
+    @State private var gender: Bool?
+    
     var body: some View {
         ZStack{
             HStack {
@@ -276,13 +323,72 @@ struct AgeGenderView: View {
             }
             VStack{
                 Description(image: image, title: title, description: description)
+                VStack{
+                    HStack{
+                        MyTypography.bodytitle(text: "나이")
+                            .frame(width: 50)
+                        
+                        BoxModifier{
+                            if let age = viewModel.user.age {
+                                Text("\(age)")
+                            }else{
+                                Text("-")
+                            }
+                        }
+                        .onTapGesture {
+                            isAgePickerPresented = true
+                        }
+                        .sheet(isPresented: $isAgePickerPresented) {
+                            PickerSheet(title: "연령대", unit: "0 대",values: ages, selectedValue: $age)
+                        }
+                        .onChange(of: age) { setAge in
+                            viewModel.user.age = setAge * 10
+                        }
+                        MyTypography.bodytitle(text: "대")
+                            .frame(width: 50)
+                    }
+                    .padding(8)
+                    
+                    // 성별
+                    HStack{
+                        MyTypography.bodytitle(text: "성별")
+                            .frame(width: 50)
+                        BoxModifier(width: 72, color: gender ?? false ? TUColor.main : TUColor.box) {
+                            Button(action: {
+                                gender = true
+                            }, label: {
+                                Text("남자")
+                                    .foregroundStyle(gender ?? false ? .black : .white)
+                            })
+                        }
+                        //.background(gender ?? false ? TUColor.main : TUColor.box)
+                        BoxModifier(width: 72, color: gender ?? true ? TUColor.box : TUColor.main) {
+                            Button(action: {
+                                gender = false
+                            }, label: {
+                                Text("여자")
+                                    .foregroundStyle(gender ?? true ? .white : .black)
+                            })
+                        }
+                        MyTypography.bodytitle(text: "")
+                            .frame(width: 50)
+                    }
+                    .animation(.easeOut(duration: 0.5), value: gender)
+                    .padding(8)
+                }
+                .padding(.top, 40)
+                Spacer()
+                TUButton(active: viewModel.user.age != nil && gender != nil, buttonText: "다음으로") {
+                    viewModel.user.gender = gender
+                    viewModel.flow = .runningStyle
+                }
             }
         }
         .padding(20)
     }
 }
 
-// MARK: - 러닝 스타일
+// MARK: - 러닝 스타일 View
 
 struct RunningStyleView: View {
     @EnvironmentObject var viewModel: SignUpViewModel
@@ -291,35 +397,8 @@ struct RunningStyleView: View {
     private let title = "러닝 스타일"
     private let description = "원하는 러닝 스타일을 설정하고 그에 맞는 피드백을 받아보세요."
     
-    var body: some View {
-        ZStack{
-            HStack {
-                Spacer()
-                VStack {
-                    Button(action: {
-                        viewModel.flow = nextflow
-                    }, label: {
-                        MyTypography.bodytitle(text: "건너뛰기")
-                    })
-                    Spacer()
-                }
-            }
-            VStack{
-                Description(image: image, title: title, description: description)
-            }
-        }
-        .padding(20)
-    }
-}
-
-// MARK: - 일일 목표량
-
-struct DailyGoalView: View {
-    @EnvironmentObject var viewModel: SignUpViewModel
-    private let nextflow: SignUpFlow = .runningStyle
-    private let image = Image(.calendarMark)
-    private let title = "일일 운동량"
-    private let description = "하루에 정해진 운동량을 설정하고 꾸준히 뛸 수 있는 이유를 만들어 주세요."
+    @State private var runningStyle: Int?
+    
     
     var body: some View {
         ZStack{
@@ -336,13 +415,145 @@ struct DailyGoalView: View {
             }
             VStack{
                 Description(image: image, title: title, description: description)
+                VStack{
+                    BoxModifier(width: 200, color: runningStyle == 0 ? TUColor.main : TUColor.box) {
+                        Button(action: {
+                            runningStyle = 0
+                        }, label: {
+                            Text("0번스타일")
+                                .foregroundStyle(runningStyle == 0 ? .black : .white)
+                        })
+                    }
+                    .padding(8)
+                    BoxModifier(width: 200, color: runningStyle == 1 ? TUColor.main : TUColor.box) {
+                        Button(action: {
+                            runningStyle = 1
+                        }, label: {
+                            Text("1번스타일")
+                                .foregroundStyle(runningStyle == 1 ? .black : .white)
+                        })
+                    }
+                    .padding(8)
+                    BoxModifier(width: 200, color: runningStyle == 2 ? TUColor.main : TUColor.box) {
+                        Button(action: {
+                            runningStyle = 2
+                        }, label: {
+                            Text("2번스타일")
+                                .foregroundStyle(runningStyle == 2 ? .black : .white)
+                        })
+                    }
+                    .padding(8)
+                    BoxModifier(width: 200, color: runningStyle == 3 ? TUColor.main : TUColor.box) {
+                        Button(action: {
+                            runningStyle = 3
+                        }, label: {
+                            Text("3번스타일")
+                                .foregroundStyle(runningStyle == 3 ? .black : .white)
+                        })
+                    }
+                    .padding(8)
+                }
+                .animation(.easeOut(duration: 0.2), value: runningStyle)
+                .padding(.top, 20)
+                Spacer()
+                TUButton(active: runningStyle != nil, buttonText: "다음으로") {
+                    // 러닝스타일 추가하면 추가로 작성
+                    viewModel.flow = .daily
+                }
             }
         }
         .padding(20)
     }
 }
 
+// MARK: - 일일 목표량 View
 
+struct DailyGoalView: View {
+    @EnvironmentObject var viewModel: SignUpViewModel
+    private let nextflow: SignUpFlow = .runningStyle
+    private let image = Image(.calendarMark)
+    private let title = "일일 운동량"
+    private let description = "하루에 정해진 운동량을 설정하고 꾸준히 뛸 수 있는 이유를 만들어 주세요."
+    
+    @State private var isProfilePublic: Bool = false
+    @State private var setDailyGoal: String = ""
+    
+    var body: some View {
+        ZStack{
+            HStack {
+                Spacer()
+                VStack {
+                    Button(action: {
+                        viewModel.flow = nextflow
+                    }, label: {
+                        MyTypography.bodytitle(text: "건너뛰기")
+                    })
+                    Spacer()
+                }
+            }
+            VStack{
+                Description(image: image, title: title, description: description)
+                HStack{
+                    
+                    MyTypography.bodytitle(text: "거리")
+                        .frame(width: 50)
+                    BoxModifier(){
+                        TextField("일일 목표", text: $setDailyGoal)
+                            .keyboardType(.numberPad)
+                    }
+                    MyTypography.bodytitle(text: "km")
+                        .frame(width: 50)
+                }
+                .padding(.top, 40)
+                
+                Spacer()
+                HStack{
+                    MyTypography.body(text: "사용자 프로필 공개 여부")
+                    Spacer()
+                    Image(systemName: isProfilePublic ? "checkmark.circle.fill": "circle")
+                        .foregroundStyle(.white)
+                }
+                .padding(5)
+                .onTapGesture {
+                    self.isProfilePublic.toggle()
+                }
+                .animation(.easeInOut(duration: 0.2), value: isProfilePublic)
+                TUButton(active: viewModel.user.age != nil && viewModel.user.gender != nil, buttonText: "다음으로") {
+                    // 러닝스타일 추가하면 추가로 작서
+                    viewModel.flow = .daily
+                }
+            }
+        }
+        .padding(20)
+    }
+}
+
+// 입력 박스
+struct BoxModifier<Content: View>: View {
+    private let content: Content
+    private let width: CGFloat
+    private var color: Color = TUColor.box
+    
+    init(@ViewBuilder content: () -> Content) {
+        self.width = 160
+        self.content = content()
+    }
+    
+    init(width: CGFloat, color: Color, @ViewBuilder content: () -> Content) {
+        self.width = width
+        self.content = content()
+        self.color = color
+    }
+    
+    var body: some View {
+        content
+            .foregroundColor(.white)
+            .padding(12)
+            .frame(width: width)
+            .background(color)
+            .cornerRadius(Constants.ViewLayoutConst.VIEW_STANDARD_CORNER_RADIUS)
+    }
+}
 
 #Preview {
     SignUpView()
